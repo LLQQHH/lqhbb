24a799d341c7923636bc0232ec85ac96a0f5ce07
lqh <997484085@qq.com>
2021-06-02 14:35:59 +0800

    创建bottomView

---
+++ app/src/main/java/com/lqh/jaxlinmaster/lqhcommon/lqhbottomtab/LqhBottomView.java
@@ -0,0 +1,10 @@
+package com.lqh.jaxlinmaster.lqhcommon.lqhbottomtab;
+
+/**
+ * Created by Linqh on 2021/6/2.
+ *
+ * @describe:
+ */
+//@CreateUidAnnotation(uid = "10100")
+public class LqhBottomView {
+}
