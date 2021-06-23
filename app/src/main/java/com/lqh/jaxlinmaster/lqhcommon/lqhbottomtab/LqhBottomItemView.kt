4d8d68bb15e6fdb5ee23c089d5610f3d92bc6949
lqh <997484085@qq.com>
2021-06-23 17:53:54 +0800

    初步完成

---
+++ app/src/main/java/com/lqh/jaxlinmaster/lqhcommon/lqhbottomtab/LqhBottomItemView.kt
@@ -0,0 +1,190 @@
+package com.lqh.jaxlinmaster.lqhcommon.lqhbottomtab
+
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.util.AttributeSet
+import android.util.TypedValue
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import com.lqh.jaxlinmaster.R
+
+/**
+ * Created by Linqh on 2021/5/27.
+ *
+ * @describe:
+ */
+//@CreateUidAnnotation(uid = "10100")
+internal class LqhBottomItemView @JvmOverloads constructor(
+    private val mContext: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : LinearLayout(mContext, attrs, defStyleAttr) {
+    init {
+        init(attrs)
+    }
+    var textColorNormal: Int = 0
+    var textColorSelected: Int = 0
+    var textSizeNormal: Int = 0
+    var textSizeSelected: Int = 0
+    var iconMargin: Int = 0
+    var normalIcon: Drawable? = null
+    var selectedIcon: Drawable? = null
+    var itemText: String? = null
+    var customLayout: View? = null
+    var defaultLayout: View? = null
+    var config: LqhBottomTab.Config? = null
+
+    var tvTitle: TextView? = null
+    var ivIcon: ImageView? = null
+    private fun init(attrs: AttributeSet?) {
+        if (attrs == null) {
+            return
+        }
+        val ta = mContext.obtainStyledAttributes(attrs, R.styleable.LqhBottomItemView)
+        textColorNormal = ta.getColor(
+            R.styleable.LqhBottomItemView_lqhitem_textColorNormal,
+            0
+        )
+        textColorSelected = ta.getColor(
+            R.styleable.LqhBottomItemView_lqhitem_textColorSelected,
+            0
+        )
+        textSizeNormal = ta.getDimensionPixelSize(
+            R.styleable.LqhBottomItemView_lqhitem_textSizeNormal,
+            0
+        )
+        textSizeSelected = ta.getDimensionPixelSize(
+            R.styleable.LqhBottomItemView_lqhitem_textSizeSelected,
+            0
+        )
+        iconMargin = ta.getDimensionPixelSize(R.styleable.LqhBottomItemView_lqhitem_iconMargin, 0)
+        normalIcon = ta.getDrawable(R.styleable.LqhBottomItemView_lqhitem_iconNormal)
+        selectedIcon = ta.getDrawable(R.styleable.LqhBottomItemView_lqhitem_iconSelected)
+        itemText = ta.getString(R.styleable.LqhBottomItemView_lqhitem_itemText)
+        var customLayoutId: Int =
+            ta.getResourceId(R.styleable.LqhBottomItemView_lqhitem_customLayoutId, 0)
+        if (customLayoutId != 0) {
+            customLayout =
+                LayoutInflater.from(mContext).inflate(customLayoutId, this, false)
+        }
+        defaultLayout =
+            LayoutInflater.from(mContext).inflate(R.layout.layout_item_lqhbottom, this, false)
+        ta.recycle()
+        initView()
+    }
+
+    //这是初始化View
+    private fun initView() {
+        tvTitle = defaultLayout!!.findViewById<TextView>(R.id.tv_title)
+        ivIcon = defaultLayout!!.findViewById<ImageView>(R.id.iv_icon)
+        if (customLayout != null) {
+            removeView(defaultLayout)
+            if (this != customLayout!!.parent) {
+                if (customLayout!!.parent != null) {
+                    (customLayout!!.parent as ViewGroup).removeView(customLayout)
+                }
+                addView(customLayout)
+            }
+        } else {
+            removeView(customLayout)
+            if (this != defaultLayout!!.parent) {
+                if (defaultLayout!!.parent != null) {
+                    (defaultLayout!!.parent as ViewGroup).removeView(defaultLayout)
+                }
+                addView(defaultLayout)
+            }
+        }
+        updateView()
+    }
+
+    public fun updateView() {
+        orientation = LinearLayout.VERTICAL
+        gravity = Gravity.CENTER
+        if (!itemText.isNullOrEmpty()) {
+            tvTitle?.text = itemText
+            tvTitle?.visibility = View.VISIBLE
+            if (normalIcon != null) {
+                if (iconMargin != 0) {
+                    (tvTitle?.layoutParams as MarginLayoutParams).topMargin = iconMargin
+                } else {
+                    if (config != null) {
+                        (tvTitle?.layoutParams as MarginLayoutParams).topMargin =config!!.iconMargin
+                    }
+                }
+            } else {
+                (tvTitle?.layoutParams as MarginLayoutParams).topMargin = 0
+            }
+        } else {
+            tvTitle?.visibility = View.GONE
+        }
+        if (textColorNormal != 0) {
+            tvTitle?.setTextColor(textColorNormal)
+        } else {
+            if (config != null) {
+                tvTitle?.setTextColor(config!!.textColorNormal)
+            }
+        }
+        if (textSizeNormal != 0) {
+            tvTitle?.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSizeNormal.toFloat())
+        } else {
+            if (config != null) {
+                tvTitle?.setTextSize(TypedValue.COMPLEX_UNIT_PX, config!!.textColorNormal.toFloat())
+            }
+        }
+        if (normalIcon != null) {
+            ivIcon?.setImageDrawable(normalIcon)
+        }
+    }
+
+    fun refreshTab(isSelected: Boolean) {
+        setSelected(isSelected)
+        if (isSelected) {
+            if (textColorSelected != 0) {
+                tvTitle?.setTextColor(textColorSelected)
+            } else {
+                if (config != null) {
+                    tvTitle?.setTextColor(config!!.textColorSelected)
+                }
+            }
+            if (textSizeSelected != 0) {
+                tvTitle?.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSizeSelected.toFloat())
+            } else {
+                if (config != null) {
+                    tvTitle?.setTextSize(
+                        TypedValue.COMPLEX_UNIT_PX,
+                        config!!.textSizeSelected.toFloat()
+                    )
+                }
+            }
+            if (selectedIcon != null) {
+                ivIcon?.setImageDrawable(selectedIcon)
+            }
+        } else {
+            if (textColorNormal != 0) {
+                tvTitle?.setTextColor(textColorNormal)
+            } else {
+                if (config != null) {
+                    tvTitle?.setTextColor(config!!.textColorNormal)
+                }
+            }
+            if (textSizeNormal != 0) {
+                tvTitle?.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSizeNormal.toFloat())
+            } else {
+                if (config != null) {
+                    tvTitle?.setTextSize(
+                        TypedValue.COMPLEX_UNIT_PX,
+                        config!!.textColorNormal.toFloat()
+                    )
+                }
+            }
+            if (normalIcon != null) {
+                ivIcon?.setImageDrawable(normalIcon)
+            }
+        }
+    }
+}
--- app/src/main/java/com/lqh/jaxlinmaster/lqhcommon/lqhbottomtab/LqhBottomTab.kt
+++ app/src/main/java/com/lqh/jaxlinmaster/lqhcommon/lqhbottomtab/LqhBottomTab.kt
@@ -2,6 +2,8 @@ package com.lqh.jaxlinmaster.lqhcommon.lqhbottomtab
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.View
+import android.view.ViewGroup
 import android.widget.LinearLayout
 import com.lqh.jaxlinmaster.R
 
@@ -10,7 +12,7 @@ import com.lqh.jaxlinmaster.R
  * @describe:
  */
 //@CreateUidAnnotation(uid = "10100")
-  class LqhBottomTab @JvmOverloads constructor(
+class LqhBottomTab @JvmOverloads constructor(
     private val mContext: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
@@ -18,20 +20,175 @@ import com.lqh.jaxlinmaster.R
     mContext, attrs, defStyleAttr
 ) {
     private var config: Config? = null
+
+    //保存所有item
+    private var mItemViews: MutableList<LqhBottomItemView> = mutableListOf()
+
+    //监听器
+    private var selectedListeners: MutableList<BaseOnTabSelectedListener> = mutableListOf()
+    private var onInterruptListener: OnInterruptListener? = null
+    private var mCurrentItem = 0 //当前条目的索引
+
+
     private fun init(attrs: AttributeSet?) {
         if (attrs == null) {
             return
         }
         val ta = mContext.obtainStyledAttributes(attrs, R.styleable.LqhBottomTab)
+        var textColorNormal = ta.getColor(
+            R.styleable.LqhBottomTab_lqhtab_item_textColorNormal,
+            UIUtils.getColor(mContext, R.color.bbl_999999)
+        )
+        var textColorSelected = ta.getColor(
+            R.styleable.LqhBottomTab_lqhtab_item_textColorSelected,
+            UIUtils.getColor(mContext, R.color.bbl_ff0000)
+        )
+        var textSizeNormal = ta.getDimensionPixelSize(
+            R.styleable.LqhBottomTab_lqhtab_item_textSizeNormal,
+            UIUtils.dp2Px(mContext, 14)
+        )
+        var textSizeSelected = ta.getDimensionPixelSize(
+            R.styleable.LqhBottomTab_lqhtab_item_textSizeSelected,
+            UIUtils.dp2Px(mContext, 14)
+        )
+        var iconMargin =
+            ta.getDimensionPixelSize(R.styleable.LqhBottomTab_lqhtab_item_iconMargin, 0)
+        config?.textColorNormal = textColorNormal
+        config?.textColorSelected = textColorSelected
+        config?.textSizeNormal = textSizeNormal
+        config?.textSizeSelected = textSizeSelected
+        config?.iconMargin = iconMargin
+        ta.recycle()
+    }
+
+    override fun addView(child: View?, index: Int, params: ViewGroup.LayoutParams?) {
+        addConfigToTab(child)
+        super.addView(child, index, params)
+    }
+
+    private fun addConfigToTab(child: View?) {
+        if (child is LqhBottomItemView) {
+            if (child.config == null) {
+                child.config = this.config
+                child.updateView()
+            }
+        }
+    }
+
+    private fun addTab(child: LqhBottomItemView) {
+        val layoutParams = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT)
+        layoutParams.weight = 1f
+        child.layoutParams = layoutParams
+        addConfigToTab(child)
+        addView(child)
+        initChild()
+    }
+
+    private fun removetab(child: LqhBottomItemView) {
+        removeView(child)
+        initChild()
     }
 
     //这个是配置类,主要是用来配置item的
-    class Config
+    class Config {
+        var textColorNormal: Int = 0
+        var textColorSelected: Int = 0
+        var textSizeNormal: Int = 0
+        var textSizeSelected: Int = 0
+        var iconMargin: Int = 0
+    }
 
     init {
         if (config == null) {
             config = Config()
         }
         init(attrs)
+    }
+
+    override fun onFinishInflate() {
+        super.onFinishInflate()
+        initChild()
+    }
+
+    //初始化开始
+    private fun initChild() {
+        mItemViews.clear()
+        if (childCount == 0) {
+            return
+        }
+        for (i in 0 until childCount) {
+            var childView = getChildAt(i)
+            if (getChildAt(i) is LqhBottomItemView) {
+                mItemViews.add(childView as LqhBottomItemView)
+                childView.setOnClickListener(LqhOnClickListener(i))
+            }
+        }
+    }
+
+    inner class LqhOnClickListener(private val clickPosition: Int) : OnClickListener {
+        override fun onClick(v: View) {
+            //判断有没有打断
+            judgeSelected(mCurrentItem, clickPosition)
+        }
+    }
+
+    interface BaseOnTabSelectedListener {
+        fun onTabSelected(position: Int)
+        fun onTabUnselected(position: Int)
+        fun onTabReselected(position: Int)
+    }
+
+    //这个用来是否打断点击事件，触发的后续事件，这个无法截断Viewpager通过手势的界面转换
+    interface OnInterruptListener {
+        fun onInterrupt(position: Int): Boolean
+    }
+
+
+    public fun judgeSelected(oldPosition: Int, newPosition: Int) {
+        if (onInterruptListener == null || onInterruptListener!!.onInterrupt(newPosition)) {
+            changeSelected(oldPosition, newPosition)
+        }
+    }
+
+    fun changeSelected(oldPosition: Int, newPosition: Int) {
+        //重置样式
+        resetStateUI()
+        //更改选中样式
+        updateSelectedTabUI(newPosition)
+        //监听都发出去
+        dispatchTabListener(oldPosition, newPosition)
+        //重新赋值
+        mCurrentItem = newPosition
+    }
+
+    //分发监听
+    private fun dispatchTabListener(oldPosition: Int, newPosition: Int) {
+        if (selectedListeners.size > 0) {
+            for (listener in selectedListeners) {
+                if (oldPosition == newPosition) {
+                    listener.onTabReselected(newPosition)
+                } else {
+                    if (oldPosition != -1) {
+                        listener.onTabUnselected(oldPosition)
+                    }
+                    listener.onTabSelected(newPosition)
+                }
+            }
+        }
+    }
+
+    private fun updateSelectedTabUI(newPosition: Int) {
+        if (mItemViews.size > newPosition) {
+            mItemViews[newPosition].refreshTab(true)
+        }
+    }
+
+    /**
+     * 重置当前按钮的状态
+     */
+    private fun resetStateUI() {
+        for (i in mItemViews.indices) {
+            mItemViews[i].refreshTab(false)
+        }
     }
 }
